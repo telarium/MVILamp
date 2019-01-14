adf671fa3d4a2b35eaae68be9537d5ca11da318d
Andrew Langley <andrew@langleycreations.com>
2019-01-14 13:56:16 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+OLD_HOST=$(hostname)
+NEW_HOST='mvimayorlamp' # Can only contain alphanumeric characters!
 
+sudo apt-get install git python-dev python-pip -y
 
-sudo apt-get install git python-dev -y
+read -p "Change hostname? [Y/n]" -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	sudo sh -c "echo $NEW_HOST > /etc/hostname"
+	sudo sed -i "s/$OLD_HOST/$NEW_HOST/g" /etc/hosts
+	printf "\nHostname set!"
+fi
+
+sudo depmod
 
 # Set PWM pins to be used as analog audio output
 echo "dtoverlay=pwm-2chan,pin=18,func=2,pin2=13,func2=4" | sudo tee -a /boot/config.txt
@@ -15,3 +26,4 @@ sudo apt-get update && sudo apt-get -y dist-upgrade && sudo rpi-update 52241088c
 
 # Enable general SPI interface as well was enabling SPI1
 printf "dtparam=spi=on\ndtoverlay=spi1-3cs" | sudo tee -a /boot/config.txt
+
